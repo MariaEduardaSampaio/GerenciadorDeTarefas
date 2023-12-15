e1c3161fd5c4979764c3c16ae3c85e3a1c6aa069
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-14 22:00:16 -0300

    cria classe usuario

---
+++ ClassLibrary/Usuarios/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary.Usuarios
+{
+    public enum AcessoAoSistema
+    {
+        TOTAL = 1,
+        PARCIAL = 2
+    }
+
+    public class Usuario(string Nome, AcessoAoSistema TipoDeAcesso, string Email, Senha senha)
+    {
+        public string? Nome { get; private set; } = Nome;
+        public AcessoAoSistema TipoDeAcesso { get; private set; } = TipoDeAcesso;
+
+        public string Email { get; private set; } = Email;
+        private Senha senha { get; set; } = senha;
+    }
+}
