2aa7cb8bdd0613928dc16c168f369828f41278d0
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-15 15:59:46 -0300

    adiciona metodos para alterar estado

--- ClassLibrary/Tarefas/Tarefa.cs
+++ ClassLibrary/Tarefas/Tarefa.cs
@@ -22,19 +22,63 @@ namespace ClassLibrary.Tarefas
         public int Id { get; private set; }
         public StatusTarefa statusTarefa {  get; private set; }
         public string EmailDoResponsavel { get; private set; }
-        public DateTime DiaCriacao { get; private set; }
-        public DateTime DiaFinalizada { get; private set; }
+        public DateTime DiaCriacao { get; private set; } = DateTime.MinValue;
+        public DateTime DiaFinalizada { get; private set; } = DateTime.MinValue;
         public string Objetivo { get; set; }
         public string Descricao { get; set; }
-
         public Tarefa(string EmailDoResponsavel, string Objetivo, string Descricao)
         {
             Id = ultimoId++;
-            statusTarefa = StatusTarefa.EM_ANALISE;
+            AnalisarTarefa();
             this.EmailDoResponsavel = EmailDoResponsavel;
-            DiaCriacao = DateTime.Now;
             this.Objetivo = Objetivo;
             this.Descricao = Descricao;
+        }
+        public void AbandonarTarefa()
+        {
+            if (DiaCriacao == DateTime.MinValue)
+                throw new ArgumentException("Não é possível abandonar uma tarefa que não foi iniciada.");
+            statusTarefa = StatusTarefa.ABANDONADA;
+            DiaFinalizada = DateTime.Now;
+            Console.WriteLine($"Tarefa {Id} abandonada... :(");
+        }
+
+        public void ImpedirTarefa()
+        {
+            if (DiaCriacao == DateTime.MinValue)
+                throw new ArgumentException("Não é possível impedir uma tarefa que não foi iniciada.");
+            statusTarefa = StatusTarefa.IMPEDIDA;
+            Console.WriteLine($"Tarefa {Id} impedida... :(");
+        }
+
+        public void AnalisarTarefa()
+        {
+            statusTarefa = StatusTarefa.EM_ANALISE;
+            Console.WriteLine($"Tarefa {Id} em análise pelo tech leader!");
+        }
+
+        public void IniciarTarefa()
+        {
+            statusTarefa = StatusTarefa.INICIADA;
+            DiaCriacao = DateTime.Now;
+            Console.WriteLine($"Tarefa {Id} iniciada pelo tech leader!");
+        }
+
+        public void ConcluirTarefa()
+        {
+            if (DiaCriacao == DateTime.MinValue)
+                throw new ArgumentException("Não é possível concluir uma tarefa que não foi iniciada.");
+            statusTarefa = StatusTarefa.CONCLUIDA;
+            DiaFinalizada = DateTime.Now;
+            Console.WriteLine($"Tarefa {Id} concluída! :)");
+        }
+
+        public void AtrasarTarefa()
+        {
+            if (DiaCriacao == DateTime.MinValue)
+                throw new ArgumentException("Não é possível atrasar uma tarefa que não foi iniciada.");
+            statusTarefa = StatusTarefa.ATRASADA;
+            Console.WriteLine($"Tarefa {Id} está atrasada :(");
         }
 
         public void ImprimirTarefa()
@@ -51,6 +95,15 @@ namespace ClassLibrary.Tarefas
             Console.WriteLine("--------------------------------------------------");
             Console.WriteLine($"Descrição: {Descricao}");
             Console.WriteLine("***************************************************");
+        }
+
+        public static void ListarStatusTarefa()
+        {
+            Console.WriteLine("Lista de Status de Tarefa:");
+            foreach (var status in Enum.GetValues(typeof(StatusTarefa)))
+            {
+                Console.WriteLine($"{(int)status}: {status}");
+            }
         }
     }
 }
