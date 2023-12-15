805a2c14634186addae83f3ad8f0a9365e14e11e
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-14 21:59:38 -0300

    cria classe senha

---
+++ ClassLibrary/Usuarios/Senha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Usuarios
+{
+    public class Senha
+    {
+        private string senha { get; set; }
+
+        public Senha(string senha)
+        {
+            ValidarSenha(senha);
+        }
+
+        private void ValidarSenha(string senha)
+        {
+            if (String.IsNullOrEmpty(senha))
+                throw new ArgumentNullException("Senha não pode ser nula.");
+            else if (senha.Length < 8)
+                throw new ArgumentException("Senha deve ter ao menos 8 caracteres.");
+            else if (!senha.Any(letra => char.IsNumber(letra)))
+                throw new ArgumentException("Senha deve conter ao menos um número.");
+            else if (!senha.Any(letra => char.IsLetter(letra)))
+                throw new ArgumentException("Senha deve conter ao menos uma letra.");
+            this.senha = senha; 
+        }
+    }
+}
