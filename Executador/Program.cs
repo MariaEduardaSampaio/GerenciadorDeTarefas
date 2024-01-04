a2530c0f799f55e2a46234aeaec3c08904939e65
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-01-03 21:32:58 -0300

    ajustes gerais

--- Executador/Program.cs
+++ Executador/Program.cs
@@ -6,16 +6,6 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            var context = new TaskManagerContext();
-            context.Tasks.Add(new TaskModel()
-            {
-                Status = 1,
-                CreatedDate = DateTime.Now,
-                Description = "descricao muito longa",
-                Objective = "objetivo",
-                Responsable = 1
-            });
-            context.SaveChanges();
         }
     }
 }
