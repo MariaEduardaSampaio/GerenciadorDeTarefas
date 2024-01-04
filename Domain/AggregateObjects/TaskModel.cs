42f0c8f3d5b4c6ae599a27f2debdf611b9c3b3a0
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-01-03 21:30:16 -0300

    modifica aggregate objects conforme requisitos

--- Domain/AggregateObjects/TaskModel.cs
+++ Domain/AggregateObjects/TaskModel.cs
@@ -3,13 +3,13 @@
     public class TaskModel
     {
         public int Id { get; set; }
-        public int? Responsable { get; set; }
-        public string EmailResponsable { get; set; }
         public int Status { get; set; }
+        public string? EmailResponsable { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string Objective { get; set; }
         public string Description { get; set; }
         public UserModel User { get; set; }
+        public int UserID { get; set; }
     }
 }
--- Domain/AggregateObjects/UserModel.cs
+++ Domain/AggregateObjects/UserModel.cs
@@ -6,8 +6,9 @@
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+        public int AccessType { get; set; }
         public int Role { get; set; }
-
+        public List<TaskModel> Tasks { get; set; }
 
     }
 }
