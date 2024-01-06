9eb03f9236370db6f80d3e088d0e6e0fd103c736
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-01-06 00:34:26 -0300

    modifica accessType para não obrigatório em userModel

--- Domain/AggregateObjects/UserModel.cs
+++ Domain/AggregateObjects/UserModel.cs
@@ -6,7 +6,7 @@
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public int AccessType { get; set; }
+        public int? AccessType { get; set; }
         public int Role { get; set; }
         public List<TaskModel> Tasks { get; set; } = new();
 
